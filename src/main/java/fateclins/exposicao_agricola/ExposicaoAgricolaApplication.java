e401b32ec279d0dc8e880114d9294e10b5888766
Edwin Yassunaga <edwinjunyassunaga@gmail.com>
2024-10-22 12:16:04 -0300

    Inicio do projeto

---
+++ src/main/java/fateclins/exposicao_agricola/ExposicaoAgricolaApplication.java
@@ -0,0 +1,13 @@
+package fateclins.exposicao_agricola;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExposicaoAgricolaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExposicaoAgricolaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fateclins/exposicao_agricola/ExposicaoAgricolaApplicationTests.java
@@ -0,0 +1,13 @@
+package fateclins.exposicao_agricola;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExposicaoAgricolaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
